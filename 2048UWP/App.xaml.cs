4c58c2167bfa1142f1ffd3546811c5df4e189f1e
HzDSN <415855355@qq.com>
2016-07-01 10:13:23 +0800

    Add Start Screen

--- 2048UWP/App.xaml.cs
+++ 2048UWP/App.xaml.cs
@@ -42,12 +42,12 @@ namespace _2048UWP
         /// <param name="e">有关启动请求和过程的详细信息。</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-#if DEBUG
+/*#if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 this.DebugSettings.EnableFrameRateCounter = true;
             }
-#endif
+#endif*/
             Frame rootFrame = Window.Current.Content as Frame;
 
             // 不要在窗口已包含内容时重复应用程序初始化，
